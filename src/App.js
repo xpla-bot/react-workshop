961963091259a1636d1ce791f61acaddc3b148c5
Loren Stewart <l.s.stewart111@gmail.com>
2016-12-28 10:25:22 -0800

    4 Essential Aspects [4 Aspects]

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import React, { Component } from 'react';
+// Anatomy of a React Component: 4 parts
 
+// 1/ (1) Import React, and the Component class
+import React, { Component } from 'react';
+// 1/ (2) Create a component of your own by extending React's component class.
 class App extends Component {
+  // 5/ (3) Create a render() method that RETURNS a single HTML element.
   render() {
     return (
-      <div className="container">
-        <p className="title">Create React App:</p>
-        <p>Getting a React project started used to be a difficult endeavor, arguably more difficult than building the indended app!</p>
-        <p>The Facebook team recognized this obstacle to using React, and developed a Command Line Interface (CLI) to make starting a React project dead simple.</p>
-        <p>If you haven't done so yet, open your terminal and:</p>
-        <pre>npm install -g create-react-app</pre>
-        <p>to install create-react-app globally.</p>
-        <hr />
-        <p>When you have create-react-app installed, navigate to where you want your project to appear and:</p>
-        <pre>create-react-app shopping-app</pre>
-        <p>This will create a 'shopping-app' directory with a React starter project.</p>
-      </div>
+      <h1>I'm a React component 🤘</h1>
     );
   }
 }
-
+// 1/ (4) Export your component for use throughout your app.
 export default App;
