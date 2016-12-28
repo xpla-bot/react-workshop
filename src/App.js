524513ffa9503932f9adaac1f833dfbddfe01dee
Loren Stewart <l.s.stewart111@gmail.com>
2016-12-28 10:22:00 -0800

    Limitations [Limitations]

--- src/App.js
+++ src/App.js
@@ -4,8 +4,20 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <p>React.js is a JavaScript library that allows you to build UI components that can be combined to make complex, interactive user interfaces (UIs).</p>
-        <p>React renders UIs very quickly. It does so by comparing the current state of the DOM with the updates state (‘diff-ing’), and only re-rendering what has been changed.</p>
+        <p className="title">Limitations:</p>
+        <ul>
+          <li>Although React components can manage state (i.e. data), it’s tools are limited. <b>Redux</b> is the most well-known <b>state management library</b> for React, it’s practically the default choice, but there are other well-supported libraries, too.</li>
+          <li>
+            React comes with neither a router nor AJAX capabilities. It’s focus is solely on rendering an interface quickly and efficiently. The default libraries for these are:
+            <ul>
+              <li><b>Router:</b> React Router</li>
+              <li><b>AJAX:</b> fetch polyfill, or Axios.</li>
+            </ul>
+          </li>
+          <li>No built-in <b>form creation and validation</b> tools</li>
+          <li>No enforced <b>conventions</b> for app structure</li>
+        </ul>
+        <p className="note">Although the above are noted as <em>limitations</em>, they can also be thought of as providing <em>freedom</em> to choose your own solutions.</p>
       </div>
     );
   }
