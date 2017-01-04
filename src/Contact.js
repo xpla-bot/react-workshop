75465d2b325091603bf9b1210f3ea3181ee626ff
Loren Stewart <l.s.stewart111@gmail.com>
2017-01-04 10:39:26 -0800

    Refactoring into Sub-Components [Refactor]

--- src/Contact.js
+++ src/Contact.js
@@ -1,40 +1,17 @@
 import React, { Component } from 'react';
 
-// 1/ Create a Contact component
 class Contact extends Component {
-  // Render some html (media object) from the Bootstrap website
+
   render() {
-    // Change 'class' to 'className' & add a closing slash to <img>
     return (
-      <div className="media">
-        <div className="media-left">
-          <a href="#">
-            <img className="media-object"
-              src="https://randomuser.me/api/portraits/med/men/85.jpg" alt="contact avatar" />
-          </a>
-        </div>
-        <div className="media-body">
-
-          <h4 className="media-heading">Jerry Jennings</h4>
-          <p><i className="fa fa-envelope-o"></i>jennings@gmail.com</p>
-          <p><i className="fa fa-phone"></i>(999) 555-6767</p>
-
-          <div className="rating">
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star-o"></i>
-          </div>
-
-        </div>
-
-        <div className="group">
-          <i className="fa fa-address-card-o"></i>
-        </div>
+      <div className="container">
+        <p>Now we need to break up the Contact component into sub-components.</p>
+        <p>This will allow us to reuse the sub-components throughout our app.</p>
+        <p>For example, the <b>avatar</b> and <b>contact details</b> could be made into sub-components. They could then be reused on a full screen Contact page that has notes and other features.</p>
+        <p>If we need to adjust the component’s style when it's used in a different context, we can do so in our CSS without changing the component’s code.</p>
       </div>
     );
   }
 }
-// 1/ export Contact
+
 export default Contact;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// 1/ import the Contact component
+
 import Contact from './Contact';
 import './style.css';
 
-// Mount the Contact component to the DOM.
+
 ReactDOM.render(
   <Contact />,
   document.getElementById('root')
