0c6616c91ec66fe34e79d42236c1c2d93fc4b7b0
Loren Stewart <l.s.stewart111@gmail.com>
2017-01-10 11:05:59 -0800

    Lifecycle Hooks and AJAX [AJAX]

--- src/App.js
+++ src/App.js
@@ -4,16 +4,7 @@ import ContactList from './ContactList';
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <p>We now have a ContactList and Contact components.</p>
-        <p>In our ContactList we need an array of data to loop over. We'll then create a Contact for every item of data in the array.</p>
-        <p>To do this we will first go over:</p>
-        <ul>
-          <li><b>AJAX</b> in React</li>
-          <li><b>Axios</b>, an AJAX library</li>
-          <li>React's <b>Lifecycle hooks</b>, which provide functions that fire at specific points in a component's life.</li>
-        </ul>
-      </div>
+      <ContactList />
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import Info from './Info';
 import './style.css';
 
 ReactDOM.render(
-  <App />,
+  <Info />,
   document.getElementById('root')
 );
